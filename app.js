bd46c99d63968a3b22070b5739b1be8f14f6fa52
BenDaG <benbrewerbowman@gmail.com>
2017-04-26 22:58:36 -0600

    Socket list attributes moved to new player list

--- app.js
+++ app.js
@@ -12,22 +12,53 @@ app.use('/client', express.static(__dirname + '/client'));
 serv.listen(2000);
 console.log("Server started.");
 
+// list of socket connections
 var SOCKET_LIST = {};
+// player list
+var PLAYER_LIST = {};
+var Player = function(id){
+  var self = {
+    x      : 250,
+    y      : 250,
+    id     : id,
+    number : "" + Math.floor(10 * Math.random()),
+    maxSpd : 10,
+    pressingRight : false,
+    pressingLeft  : false,
+    pressingUp    : false,
+    pressingDown  : false
+  }
+  self.updatePosition = function() {
+    if(self.pressingRight)
+      self.x += self.maxSpd;
+    if(self.pressingLeft)
+      self.x -= self.maxSpd;
+    if(self.pressingUp)
+      self.y -= self.maxSpd;
+    if(self.pressingDown)
+      self.y += self.maxSpd;
+  }
+  return self;
+}
+
 // loads file and initializes it. Returns io obj with all the functionalities of io socket library
 var io = require('socket.io') (serv,{});
 // when connection present, this function is called, display msg
 io.sockets.on('connection', function(socket){
   // generate random player connect ID
   socket.id = Math.random();
-  socket.number = "" + Math.floor(10 * Math.random());
-  // init player coords
-  socket.x = 0;
-  socket.y = 0;
   // add player to list of online sockets
   SOCKET_LIST[socket.id] = socket;
 
+  var player = Player(socket.id);
+  PLAYER_LIST[socket.id] = player;
+
+  // when socket disconnect
   socket.on('disconnect',function(){
+    // delete from socket connection list
     delete SOCKET_LIST[socket.id];
+    // delete player from player list
+    delete PLAYER_LIST[socket.id];
   });
 });
 
@@ -35,15 +66,15 @@ io.sockets.on('connection', function(socket){
 setInterval(function(){
   var pack = [];
   // loop through every socket in list
-  for(var i in SOCKET_LIST){
+  for(var i in PLAYER_LIST){
     // increase the location of each socket by 1 x&y
-    var socket = SOCKET_LIST[i];
-    socket.x++;
-    socket.y++;
+    var player = PLAYER_LIST[i];
+    player.x++;
+    player.y++;
     pack.push({
-      x: socket.x,
-      y: socket.y,
-      number: socket.number
+      x      : player.x,
+      y      : player.y,
+      number : player.number
     });
   }
   for(i in SOCKET_LIST){

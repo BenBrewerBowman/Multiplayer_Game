8dda9710024736528b8a5d617724f494ecee12df
BenDaG <benbrewerbowman@gmail.com>
2017-04-30 14:18:43 -0600

    CSS and JS moved from index to files

---
+++ client/js/main.js
@@ -0,0 +1,53 @@
+var chatText = document.getElementById('chat-text');
+var chatInput = document.getElementById('chat-input');
+var chatForm = document.getElementById('chat-form');
+
+var ctx = document.getElementById("ctx").getContext("2d");
+ctx.font = '30px Arial';
+
+// socket connection between server and client
+var socket = io();
+
+socket.on('newPositions',function(data){
+  // clear canvas
+  ctx.clearRect(0,0,500,500);
+  for(var i = 0; i<data.player.length; ++i)
+    // write letter p with x and y data
+    ctx.fillText(data.player[i].number, data.player[i].x, data.player[i].y);
+  for(var i = 0; i<data.bullet.length; ++i)
+    // write letter p with x and y data
+    ctx.fillRect(data.bullet[i].x-5, data.bullet[i].y-5, 10, 10);
+});
+
+socket.on('addToChat', function(data){
+  chatText.innerHTML += '<div>' + data + '</div>';
+});
+// Enter on chat form
+chatForm.onsubmit = function(e){
+  // prevent page from refreshing
+  e.preventDefault();
+  // sends message to server equal to chat value entered
+  socket.emit('sendMsgToServer',chatInput.value);
+  chatInput.value = '';
+}
+
+document.onkeydown = function(event){
+  if(event.keyCode === 68)          // d
+    socket.emit('keyPress',{inputId:'right',state:true});
+  else if(event.keyCode === 83)     // s
+    socket.emit('keyPress',{inputId:'down',state:true});
+  else if(event.keyCode === 65)     // a
+    socket.emit('keyPress',{inputId:'left',state:true});
+  else if(event.keyCode === 87)     // w
+    socket.emit('keyPress',{inputId:'up',state:true});
+}
+document.onkeyup = function(event){
+  if(event.keyCode === 68)          // d
+    socket.emit('keyPress',{inputId:'right',state:false});
+  else if(event.keyCode === 83)     // s
+    socket.emit('keyPress',{inputId:'down',state:false});
+  else if(event.keyCode === 65)     // a
+    socket.emit('keyPress',{inputId:'left',state:false});
+  else if(event.keyCode === 87)     // w
+    socket.emit('keyPress',{inputId:'up',state:false});
+}

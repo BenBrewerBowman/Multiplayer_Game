0706800a3eb403d62f80112aebf528da7e17976d
BenDaG <benbrewerbowman@gmail.com>
2017-04-29 20:06:30 -0600

    created entitity (player + items), player list changed from global to member, player connections moved to functions

--- app.js
+++ app.js
@@ -14,52 +14,65 @@ console.log("Server started.");
 
 // list of socket connections
 var SOCKET_LIST = {};
-// player list
-var PLAYER_LIST = {};
-var Player = function(id){
+
+// everything that the player and bullet share
+var Entity = function() {
   var self = {
-    x      : 250,
-    y      : 250,
-    id     : id,
-    number : "" + Math.floor(10 * Math.random()),
-    maxSpd : 10,
-    pressingRight : false,
-    pressingLeft  : false,
-    pressingUp    : false,
-    pressingDown  : false
+    x    : 250,
+    y    : 250,
+    spdX : 0,
+    spdY : 0,
+    id   : "",
   }
-  self.updatePosition = function() {
-    if(self.pressingRight)
-      self.x += self.maxSpd;
-    if(self.pressingLeft)
-      self.x -= self.maxSpd;
-    if(self.pressingUp)
-      self.y -= self.maxSpd;
-    if(self.pressingDown)
-      self.y += self.maxSpd;
+  self.update = function(){
+    self.updatePosition();
+  }
+  self.updatePosition = function(){
+    self.x += self.spdX;
+    self.y += self.spdY;
   }
   return self;
 }
 
-// loads file and initializes it. Returns io obj with all the functionalities of io socket library
-var io = require('socket.io') (serv,{});
-// when connection present, this function is called, display msg
-io.sockets.on('connection', function(socket){
-  // generate random player connect ID
-  socket.id = Math.random();
-  // add player to list of online sockets
-  SOCKET_LIST[socket.id] = socket;
+var Player = function(id){
+  var self = Entity();
+  self.id            = id;
+  self.number        = "" + Math.floor(10 * Math.random());
+  self.maxSpd        = 10;
+  self.pressingRight = false;
+  self.pressingLeft  = false;
+  self.pressingUp    = false;
+  self.pressingDown  = false;
 
+  var super_update = self.update;
+  self.update = function(){
+    self.updateSpd();
+    super_update();
+  }
+  self.updateSpd = function() {
+    // if left or right
+    if(self.pressingRight)
+      self.spdX = self.maxSpd;
+    else if(self.pressingLeft)
+      self.spdX = -self.maxSpd;
+    else
+      self.spdX = 0;
+    // if up or down
+    if (self.pressingDown)
+      self.spdY = self.maxSpd;
+    else if(self.pressingUp)
+      self.spdY = -self.maxSpd;
+    else
+      self.spdY = 0;
+  }
+  // add player to list
+  Player.list[id] = self;
+  return self;
+}
+// list of players
+Player.list = {};
+Player.onConnect = function(socket){
   var player = Player(socket.id);
-  PLAYER_LIST[socket.id] = player;
-
-  // when socket disconnect
-  socket.on('disconnect',function(){
-    // delete from socket connection list
-    delete SOCKET_LIST[socket.id];
-    // delete player from player list
-    delete PLAYER_LIST[socket.id];
-  });
 
   // when a key pressed down
   socket.on('keyPress',function(data){
@@ -76,26 +89,51 @@ io.sockets.on('connection', function(socket){
     else if(data.inputId === 'down')
       player.pressingDown = data.state;
   });
-});
-
-// function called every .025 ms
-setInterval(function(){
+}
+Player.onDisconnect = function(socket){
+  // delete player from player list
+  delete Player.list[socket.id];
+}
+Player.update = function(){
   var pack = [];
   // loop through every socket in list
-  for(var i in PLAYER_LIST){
+  for(var i in Player.list){
     // increase the location of each socket by 1 x&y
-    var player = PLAYER_LIST[i];
-    player.updatePosition();
+    var player = Player.list[i];
+    player.update();
     pack.push({
       x      : player.x,
       y      : player.y,
       number : player.number
     });
   }
+  return pack;
+}
+
+// loads file and initializes it. Returns io obj with all the functionalities of io socket library
+var io = require('socket.io') (serv,{});
+// when connection present, this function is called, display msg
+io.sockets.on('connection', function(socket){
+  // generate random player connect ID
+  socket.id = Math.random();
+  // add player to list of online sockets
+  SOCKET_LIST[socket.id] = socket;
+
+  Player.onConnect(socket);
+  // when socket disconnect
+  socket.on('disconnect',function(){
+    // delete from socket connection list
+    delete SOCKET_LIST[socket.id];
+    Player.onDisconnect(socket);
+  });
+});
+
+// function called every .025 ms
+setInterval(function(){
+  var pack = Player.update();
   for(i in SOCKET_LIST){
     var socket = SOCKET_LIST[i];
     // send package to user containing new pos
     socket.emit('newPositions',pack);
   }
-
 },1000/25);
